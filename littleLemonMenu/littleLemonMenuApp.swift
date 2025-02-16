9248f17da923fb59877e6faa5cc7e7a690dede5b
hellshark <yahir.lope@icloud.com>
2025-02-16 15:47:43 -0600

    Initial Commit

---
+++ littleLemonMenu/littleLemonMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  littleLemonMenuApp.swift
+//  littleLemonMenu
+//
+//  Created by Yahir Lope on 16/02/25.
+//
+
+import SwiftUI
+
+@main
+struct littleLemonMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
